03e2ce316eb921824a1f64d604e9369c5ee89417
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-04-12 21:17:40 -0500

    cleaned up code

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-import praw
-import os
-import requests
-import json
-
+import praw, os, requests, json
 from dotenv import load_dotenv
 from praw.models import MoreComments
 
@@ -57,22 +53,24 @@ data = {
      "temperature": 0
    }
 
-# response = requests.post(url, headers=headers, json=data)
+response = requests.post(url, headers=headers, json=data)
 
-# if response.status_code == 200:
-#     # Parse the JSON response
-#     response_json = response.json()
+if response.status_code == 200:
+    # Parse the JSON response
+    response_json = response.json()
 
-#     # Extract the content from the response
-#     content = response_json['choices'][0]['message']['content']
-#     albums = eval(content)
-#     # print(albums[0])
-#     # Print or return the content
-# else:
-#     # Print an error message if the request failed
-#     print('Error:', response.text)
+    # Extract the content from the response
+    content = response_json['choices'][0]['message']['content']
 
-albums = ['Slint Spiderland', 'Lingua Ignota Caligula', 'A promise xiu xiu', 'El-P’s I’ll Sleep When You’re Dead', 'Yo La Tengo I Can Heart the Heart Beating as One', 'Red King Crimson', 'Koyaanisqatsi by Philip Glass', 'Talk Talk Laughing Stock', 'The Microphones The Glow, Pt 2', 'my bloody valentine loveless', 'Glenn Branca The Ascension', 'Natural Snow Buildings The Dance of the Moon and the Sun', 'nine inch nails the fragile', 'Death Grips The Money Store', 'Radiohead Kid A', 'Low Secret Name', 'To Be Cruel Khantae', 'Laibach sketches of the red district', 'manic street preachers the holy bible', 'Lift Your Skinny Dick Like Antenna To Heaven', 'Pink Floyd Wish You Were Here', 'SPELLLING The Turning Wheel']
+    # maybe change this to json.loads
+    albums = eval(content)
+    # print(albums[0])
+    # Print or return the content
+else:
+    # Print an error message if the request failed
+    print('Error:', response.text)
+
+# albums = ['Slint Spiderland', 'Lingua Ignota Caligula', 'A promise xiu xiu', 'El-P’s I’ll Sleep When You’re Dead', 'Yo La Tengo I Can Heart the Heart Beating as One', 'Red King Crimson', 'Koyaanisqatsi by Philip Glass', 'Talk Talk Laughing Stock', 'The Microphones The Glow, Pt 2', 'my bloody valentine loveless', 'Glenn Branca The Ascension', 'Natural Snow Buildings The Dance of the Moon and the Sun', 'nine inch nails the fragile', 'Death Grips The Money Store', 'Radiohead Kid A', 'Low Secret Name', 'To Be Cruel Khantae', 'Laibach sketches of the red district', 'manic street preachers the holy bible', 'Lift Your Skinny Dick Like Antenna To Heaven', 'Pink Floyd Wish You Were Here', 'SPELLLING The Turning Wheel']
 
 
 
@@ -107,7 +105,8 @@ url = 'https://api.spotify.com/v1/search'
 # album = "Rubber Soul"
 # artist = "Beatles"
 # Define the search query
-query = f'{albums[0]}'
+album = albums[19]
+query = f'{album}'
 
 # Define the parameters for the GET request
 params = {
@@ -124,7 +123,7 @@ if response.status_code == 200:
     # Extract the album details from the response JSON
     album_details = response.json()['albums']['items'][0]
     album_id = album_details['id']
-    print(f"album id for {albums[0]} is {album_id}")
+    print(f"album id for {album} is {album_id}")
 else:
     # Print an error message if the request failed
     print("Failed to search for album:", response.text)
