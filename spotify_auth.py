0de5da1e7f73d6eaddc7d1e493dd4719e4ee327a
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-04-20 14:55:46 -0500

    separated spotify auth and token

--- spotify_auth.py
+++ spotify_auth.py
@@ -16,28 +16,6 @@ auth_headers = {
     "scope": "user-library-read playlist-modify-private playlist-modify-public"
 }
 
-# webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_headers))
+webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_headers))
 
 # code will be in url of redirect window
-
-code = "AQA1QXa_t7cvUv5fMtVqsRG0O8R4KFSUOwAYbhg1HAqniLZJHZyyNbUrKJoJozFGWzJPBFvctpRHoPOejo0UcRZSPPUcEodWwaEVnn05-h2WmPoe1I95kAG0IKkwX-Dhi9i9VIMNSpGKAbII5VDtHlwtVreZjUM1_OYoUvoq21lG7406IMikpk1LFpCWRitYb9Ywkpy592wq-XNfLN2lYTJSUfqpeP-jVf3XbzLJ9LqcBKJ40uHz-Ufo8-3MIqA9a8fubngq"
-
-
-encoded_credentials = base64.b64encode(client_id.encode() + b':' + client_secret.encode()).decode("utf-8")
-
-token_headers = {
-    "Authorization": "Basic " + encoded_credentials,
-    "Content-Type": "application/x-www-form-urlencoded"
-}
-
-token_data = {
-    "grant_type": "authorization_code",
-    "code": code,
-    "redirect_uri": "http://localhost:8080/callback"
-}
-
-r = requests.post("https://accounts.spotify.com/api/token", data=token_data, headers=token_headers)
-
-token = r.json()["access_token"]
-
-print(token)
