802164ea92816f0e006f10752ac69b86924ebf59
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-04-20 14:42:53 -0500

    add multiple albums

--- get_album_tracks.py
+++ get_album_tracks.py
@@ -4,24 +4,27 @@ from praw.models import MoreComments
 
 load_dotenv()
 
-album_id = "2NnkLRaeX33d1Mn8ZLgTo8"
-
 access_token = "BQCb7nd_mxOmxxSJZGoSI_LP9vbvFomCAge5OzgPVSMjmEK4KXB7VKm1RRrHsoYAoCiXrXDjqhaGGPcyywemBeV2tfGjwFT7slwgGOURiLMmD9x4Fr-O0IrxDdgE2b-0inKjo1YfEKiknkXfUDwdFc8QaELuDYvFrOM3oTWsyy3NyOyb92NnbBSTIS16QowKTfjHr-0V58eRnLuiU8VirZTyztpPWqyrWiFuj-2nOzHyU00UL-dyNRdvCQ"
 
-url = f"https://api.spotify.com/v1/albums/{album_id}/tracks"
-headers = {
-    "Authorization": f"Bearer {access_token}"
-}
-
-response = requests.get(url, headers=headers)
-
-tracks = response.json()
+album_ids = ['2NnkLRaeX33d1Mn8ZLgTo8', '2GBJg7nTQs3go1w24ECKvy', '44CB4Ho4QCPH4oSGnY6yat', '6gZudQwiObndOhXqqDx5GD']
 
 track_ids = []
 
-# Iterate over the items in the response JSON and extract the track IDs
-for item in tracks['items']:
-    track_ids.append(item['id'])
+for album_id in album_ids:
+
+    url = f"https://api.spotify.com/v1/albums/{album_id}/tracks"
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    tracks = response.json()
+
+    # Iterate over the items in the response JSON and extract the track IDs
+    for item in tracks['items']:
+        track_ids.append(item['id'])
+
 
 # Print the list of track IDs
 print(track_ids)
