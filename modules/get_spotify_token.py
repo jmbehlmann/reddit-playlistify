ac8a7b1677cfb10ee664d670aff225b8c94593d1
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-04-26 18:54:45 -0500

    modify get token

--- modules/get_spotify_token.py
+++ modules/get_spotify_token.py
@@ -9,14 +9,14 @@ load_dotenv()
 client_id = os.environ.get('SPOTIFY_CLIENT_ID')
 client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
 
-# auth_headers = {
-#     "client_id": client_id,
-#     "response_type": "code",
-#     "redirect_uri": "http://localhost:8080/callback",
-#     "scope": "user-library-read playlist-modify-private playlist-modify-public"
-# }
+auth_headers = {
+    "client_id": client_id,
+    "response_type": "code",
+    "redirect_uri": "http://localhost:8080/callback",
+    "scope": "user-library-read playlist-modify-private playlist-modify-public"
+}
 
-# webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_headers))
+webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_headers))
 
 # code will be in url of redirect window
 
